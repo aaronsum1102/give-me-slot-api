c6ad0dbde063eb61f3a22979282e9c48625d17f6
Shao Xiang Sum <sumsx03@gmail.com>
2022-12-22 22:24:43 +0100

    feat: migrate to vercel app

--- esbuild.js
+++
@@ -1,9 +0,0 @@
-require('esbuild').buildSync({
-  entryPoints: ['src/app.ts'],
-  bundle: true,
-  minify: true,
-  treeShaking: true,
-  platform: 'node',
-  target: ['node16'],
-  outfile: '.esbuild/index.js'
-});
